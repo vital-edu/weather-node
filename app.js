ba919407f53ae66a10eac83fccea63935eb42208
Eduardo Vital <vitaldu@gmail.com>
2016-11-24 00:00:51 -0200

    Using Yargs to dinamically set address to fetch
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    address: {
+      alias: 'a',
+      describe: 'Address to search',
+      require: true,
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Endereço: ${body.results[0].formatted_address}`);
+  console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 })
