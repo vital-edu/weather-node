fbb5deabdb8166c71fa4523f7b3895f3b2f67fba
Eduardo Vital <vitaldu@gmail.com>
2016-11-24 13:42:35 -0200

    Handling errors
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ request({
   url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.log('Unable to fetch data from Google servers');
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  } else {
+    console.log(body.status);
+  }
 })
