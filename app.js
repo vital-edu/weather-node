3d860eb0393cbbde074f88791eb5bd97e4abeec8
Eduardo Vital <vitaldu@gmail.com>
2016-11-25 22:28:14 -0200

    Modularizing geocode request
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
   .options({
@@ -14,19 +15,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
+geocode.geocodeAddress(argv.address, (error, results) => {
   if (error) {
-    console.log('Unable to fetch data from Google servers');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    console.log(error);
   } else {
-    console.log(body.status);
+    console.log(JSON.stringify(results, undefined, 2));
   }
-})
+});
