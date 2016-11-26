b3d5e5cbb504c00dc5510f1e9ea47c75a530aa13
Eduardo Vital <vitaldu@gmail.com>
2016-11-26 16:27:04 -0200

    Getting weather from DarkSky api
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs
   .options({
@@ -19,6 +20,13 @@ geocode.geocodeAddress(argv.address, (error, results) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log(results.address);
+    weather.getWeather(results, (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`It's currently ${results.temperature}. It feels like ${results.apparentTemperature}.`);
+      }
+    });
   }
 });
